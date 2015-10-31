d97b5658d3cc209f7bdc3e9dc3459f3af30a8260
Şeyma Bayrak <sheymaba@gmail.com>
2015-10-31 15:15:26 +0100

    packaging the hcp_corr repo

---
+++ hcp_corr/__init__.py
@@ -0,0 +1,1 @@
+from .hcp_util import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hcp_corr',
+      version='0.0.1',
+      description='Human Connectome Project correlation matrix utilities',
+      url='https://github.com/NeuroanatomyAndConnectivity/hcp_corr',
+      author='Seyma Bayrak',
+      author_email='sheymaba@gmail.com',
+      license='MIT',
+      packages=['hcp_corr'],
+      zip_safe=False)
