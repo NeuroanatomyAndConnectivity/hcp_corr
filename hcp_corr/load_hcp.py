469059fb536a747b070974a314494317461b4575
Şeyma Bayrak <sheymaba@gmail.com>
2015-11-11 19:36:49 +0100

    minimize commits

--- hcp_corr/load_hcp.py
+++ hcp_corr/load_hcp.py
@@ -10,44 +10,22 @@ def t_series(subject = "",
     module. The HCP data is released in GIFTI format (*nii extention) for 
     almost 500 subjects. This script aims to get concetanation of all 
     time-series for each subject. 
-  
-    data_path : string
-        Local path for the HCP data
-        e.g. data_path = '/a/documents/connectome/_all'
     
     subject : string
-         Subject ID numbers of individuals scanned for the HCP. 
-         e.g. subject='100307'
+         subject = data_path + subject_id. 
+         e.g. subject = '/a/documents/connectome/_all/100307'
      
     template : string
-        Template is the sketch-name of *.nii files (GIFTI format) of interest
-        for a subject. Each subject directory has many *.nii files, but we are 
-        pointing the ones of our interest with this template string. 
-        (e.g. template = 'rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii' ,
-        this will be converted into these forms after loop:
-       
-        rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii
-        rfMRI_REST1_RL_Atlas_hp2000_clean.dtseries.nii
-        rfMRI_REST2_LR_Atlas_hp2000_clean.dtseries.nii
-        rfMRI_REST2_RL_Atlas_hp2000_clean.dtseries.nii )
+        Template is the sketch-name of *.nii files (GIFTI format), it is hard-
+        coded as template_flag and template_orig...
         
     cnt_files : int
         Number of *.nii files of interest for a subject. The template above
         has 4 forms in total, therefore cnt_files = 4
    
-   subject_path : string
-       If template requires additional path attachment before it.
-       e.g. subject_path = 'MNINonLinear/Results/rfMRI_REST?_??'
-    
-    (full address of an *nii data = 'data_path/subject/subject_path/template',
-    users are recommended to check this manually!!!)
-
-    N_cnt : int
-        User-defined number of brain nodes, specially good for test runs.
-
-    N_first : int
-        User-defined first brain node we want to get data from.
-
+    hemisphere : string
+        'LH' by default, meaning left hemisphere
+        
     K : output, numpy.ndarray
         Concetanation of time-series matrices obtained from each *.nii file. 
     
@@ -111,15 +89,14 @@ def t_series(subject = "",
                 print "Warning, %s contains time series of different length" % (subject)
             if  n_last != n:
                 print "Warning, %s contains different count of brain nodes" % (subject)
-            K.resize([n, K.shape[1] + m])
+            K.resize([n, K.shape[1]+m])
 
         # concatenation of (normalized) time-series, column-wise
         if normalize:
-            
             mean_series = single_t_series.mean(axis=1)
             std_series = single_t_series.std(axis=1)
             K[:, -m:] = ((single_t_series.T - mean_series) / std_series).T            
-                
+
         else:
             K[:, -m:] = single_t_series
         del img
